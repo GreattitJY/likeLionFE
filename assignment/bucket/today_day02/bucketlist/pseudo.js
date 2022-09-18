8dccd79b87e728f652c9713bf6de91d44785e38d
GreattitJY <great.tit.jaejae@gmail.com>
2022-09-18 12:59:05 +0900

    파일명 수정

--- assignment/bucket/today_day02/bucketlist/pseudo.js
+++
@@ -1,6 +0,0 @@
-let elements = document.querySelectorAll(".pseudo-padding");
-elements.forEach( (element) => {
-    element.addEventListener('click', function handleClick(e) {
-        e.target.classList.toggle("is_active");
-    }); 
-});
