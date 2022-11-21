54e71afb9a814e1eea5cdc3c1f14ac72bcb48293
GreattitJY <great.tit.jaejae@gmail.com>
2022-11-21 16:04:43 +0900

    React lazy_initialize

---
+++ React/036_lazy_initalize/my-app/src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useLayoutEffect } from 'react'
+
+function App() {
+  const [value, setValue] = useState(100);
+  
+  useLayoutEffect(() => {
+    if (value >= 1000) {
+      setValue(300);
+    }
+  }, [value]);
+  
+  return (
+    <div>
+      {/* <div style={{ width: value, height: value, backgroundColor: 'blue', transition: '1s all' }}></div> */}
+      <div style={{ width: value, height: value, backgroundColor: 'blue' }}></div>
+      <button onClick={() => {setValue(1000)}}>커져랏!</button>
+      <button onClick={() => {setValue(200)}}>작아져랏!</button>
+    </div>
+  )
+}
+
+export default App;
+
+// 1. useLayoutEffect는 useEffect보다 먼저 실행됩니다. 따라서 useEffect보다 먼저 실행하고 싶은 무언가가 있다면 useLayoutEffect를 사용해야 한다라는 것을 알 수 있습니다.
+
+// 2. 
+// 2.1 렌더링 되었을 때 useLayoutEffect return의 콜백함수가 실행됩니다. 그리고 useLayoutEffect 가 실행됩니다.
+// 2.2 useEffect return의 콜백함수가 실행됩니다. useEffect 가 실행됩니다.
+
+// 3. 깜빡임을 해결하고 싶다? useEffect -> useLayoutEffect
